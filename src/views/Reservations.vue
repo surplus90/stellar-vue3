101fa135f335abf633d31f5b37080e9540d9ce84
sooyoonhan <surplus90@gmail.com>
2022-02-11 23:15:24 +0900

    선택한 카드 보기

--- src/views/Reservations.vue
+++ src/views/Reservations.vue
@@ -8,7 +8,6 @@
       :rows="rows"
       :columns="columns"
       row-key="idx"
-      hide-pagination
     >
 
       <template v-slot:body-cell-userLink="props">
@@ -17,8 +16,38 @@
           <!-- <q-btn flat round color="black" icon="content_copy" size="sm" @click="copyLink(props.value)" /> -->
         </q-td>
       </template>
+      <template v-slot:body-cell-setcardsAt="props">
+        <q-td :props="props">
+          {{ props.value }}
+          <q-btn round color="purple" glossy icon="style" size="10px" v-if="props.value" @click="showCards(props.row)" /> 
+        </q-td>
+      </template>
 
     </q-table>
+
+    <q-dialog v-model="alert">
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section>
+          <div class="text-h6">{{ detail.userName }}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none" v-for="(card, num) in this.detail.cards" :key="num">
+          <q-card
+            class="my-card text-white"
+            style="background: radial-gradient(circle, #5a3b54 0%, #403a48 100%)"
+          >
+            <q-card-section>
+              <div class="text-h6">{{card}}</div>
+            </q-card-section>
+          </q-card>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
   </div>
 </template>
 
@@ -54,7 +83,14 @@ export default {
     return {
       location,
       columns,
-      rows
+      rows,
+
+      alert: ref(false)
+    }
+  },
+  data() {
+    return { 
+      detail: {}
     }
   },
   methods: {
@@ -63,6 +99,14 @@ export default {
     },
     goSetCard () {
       this.$router.push('/set/cards')
+    },
+    showCards (data) {
+      this.detail = data
+      this.$http.get(`/api/fortune-telling/show-cards/${data.idx}`).then(res => {
+        Object.assign(this.detail, { cards : res.data })
+      }).then(() => {
+        this.alert = true
+      })
     }
   },
 }
@@ -71,5 +115,10 @@ export default {
 <style scoped>
 .btn {
   margin-bottom: 15px;
+}
+.my-card {
+  width: 70px;
+  height: 100px;
+  text-align: center;
 }
 </style>
